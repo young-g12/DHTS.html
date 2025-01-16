4b7e51a57de8716b975c458b266116366e8f4966
young-g12 <74071824+young-g12@users.noreply.github.com>
2025-01-16 15:31:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ document
   });
 
 //Backend code
+//event handler for parsing js to json
 
 document.addEventListener("DOMContentLoaded", function () {
   const calendarEl = document.getElementById("calendar");
